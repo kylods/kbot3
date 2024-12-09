66c2b6f9170fcd3670377a82a547a78aca5e86b2
kylods <kylodss@gmail.com>
2024-12-08 23:34:47 -0600

    API upload endpoint

--- internal/apihandler/server.go
+++ internal/apihandler/server.go
@@ -3,8 +3,11 @@ package apihandler
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/kylods/kbot3/internal/discordclient"
@@ -20,12 +23,51 @@ type Server struct {
 	httpServer    *http.Server
 }
 
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Upload hit!")
+
+	// Parse our multipart form, 10 << 24 specifies a max
+	// upload of 160MB files
+	r.ParseMultipartForm(10 << 24)
+	// FormFile returns the first file for the given key 'audioFile'
+	// It also returns the FileHeader so we can grab the Filename,
+	// the header & the size of the file
+	file, handler, err := r.FormFile("audioFile")
+	if err != nil {
+		fmt.Println("Error retrieving the file")
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+	fmt.Printf("File Size: %+v\n", handler.Size)
+	fmt.Printf("MINE Header: %+v\n", handler.Header)
+
+	// Create a temp file that follows a particular naming pattern
+	tempFile, err := os.CreateTemp("temp-audio", "upload-")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer tempFile.Close()
+
+	// Read the contents of our file into a byte array,
+	// then write it to the tempfile
+	fileBytes, err := io.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+	tempFile.Write(fileBytes)
+
+	respondJSON(w, 202, map[string]string{"message": "Upload Successful"})
+}
+
 // Initializes a new APi server
 func NewServer(port string, discordClient *discordclient.Client, db *gorm.DB) *Server {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /ping", pingHandler)
 	mux.HandleFunc("GET /auth", authenticateHandler)
+	mux.HandleFunc("POST /upload", uploadFile)
 
 	server := http.Server{
 		Addr:         ":" + port,
